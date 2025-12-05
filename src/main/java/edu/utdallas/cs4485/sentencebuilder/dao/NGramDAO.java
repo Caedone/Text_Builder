aa1a3d34141de66f8e18d9093d399c3b3c171d57
Caedone <149904278+Caedone@users.noreply.github.com>
2025-12-04 20:59:02 -0600

    Fix class declaration in NGramDAO.java

--- src/main/java/edu/utdallas/cs4485/sentencebuilder/dao/NGramDAO.java
+++ src/main/java/edu/utdallas/cs4485/sentencebuilder/dao/NGramDAO.java
@@ -21,7 +21,7 @@ import edu.utdallas.cs4485.sentencebuilder.model.NGram;
  * @author CS4485 Team
  * @version 1.0
  */
-public class git add .NGramDAO {
+public class NGramDAO {
 
     private DatabaseConnection dbConnection;
 
